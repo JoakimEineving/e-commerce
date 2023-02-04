1199292a4bc08e5e3c71191909492b60eee285d5
joakimeineving <Joakim.eineving@gmail.com>
2023-02-04 09:01:18 +0100

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/navbar/navbar';
+import React from "react";
+import './App.css'
+
+function App() {
+  document.title = "E-commerce";
+
+  return (
+    <div className='app'>
+    <Navbar />
+    </div>
+  )
+}
+
+export default App
