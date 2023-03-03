4ff29e2f0b292a17b0efd6e9fc947e083e8d8ed4
joakimeineving <Joakim.eineving@gmail.com>
2023-03-03 09:07:10 +0100

    Login button and form

---
+++ client/src/components/LoginRegister/LoginRegister.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import accountService from "../../services/accountService";
+
+const LoginRegister = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signUp, setSignUp] = useState(false);
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    console.log('submit clicked');
+    try {
+      const accountData = { email, password };
+      const createdAccount = await accountService.signIn(accountData);
+      console.log(createdAccount);
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    console.log('submit clicked');
+    try {
+      const accountData = { email, password };
+      const createdAccount = await accountService.signUp(accountData);
+      console.log(createdAccount);
+    } catch (error) {
+      console.error(error);
+    } 
+  };
+  return (
+    <div>
+      <label htmlFor="my-modal-4" className="btn">
+        Login
+      </label>
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+        <label className="modal-box relative">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white justify-center">
+              {signUp ? "Sign Up" : "Sign In"}
+            </h1>
+            <form
+              onSubmit={signUp ? handleSignUp : handleSignIn}
+              className="space-y-4 md:space-y-6"
+              action="#"
+            >
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@emailprovider.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="••••••••"
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="remember"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <a
+                  href="#"
+                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  {!signUp &&"Forgot Password?"}
+                </a>
+              </div>
+              <button
+                onClick={signUp ? handleSignUp : handleSignIn}
+                className="btn-primary w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                {signUp ? "Sign Up" : "Sign In"}
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                {signUp ? "Already have an account?" : "Don't have an account?"}
+                <a
+                  onClick={() => setSignUp(!signUp)}
+                  href="#"
+                  className="font-medium text-primary-600 dark:text-primary-500 "
+                >
+                  {signUp ? " Sign In" : " Sign Up"}
+                </a>
+              </p>
+            </form>
+          </div>
+        </label>
+      </label>
+    </div>
+  );
+};
+
+export default LoginRegister;
--- client/src/components/Shared/Navbar.jsx
+++ client/src/components/Shared/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
+import { LoginRegister } from "../index";
 
 const Navbar = () => {
-  const cartItems = useSelector(state => state.cart);
+  const cartItems = useSelector((state) => state.cart);
   const cartItemsNum = cartItems.length;
-  const cartTotal = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  const cartTotal = cartItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
 
   return (
     <div className="sticky top-0 z-50 navbar bg-base-200 shadow-md">
       <div className="flex-1">
-      <Link to="/">
-        <span className="btn btn-ghost normal-case text-xl">E-com store</span>
-      </Link>
+        <Link to="/">
+          <span className="btn btn-ghost normal-case text-xl">E-com store</span>
+        </Link>
       </div>
+      <LoginRegister />
+      
       <div className="flex-none">
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
@@ -33,7 +38,9 @@ const Navbar = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge badge-sm indicator-item">{cartItemsNum}</span>
+              <span className="badge badge-sm indicator-item">
+                {cartItemsNum}
+              </span>
             </div>
           </label>
           <div
@@ -41,12 +48,16 @@ const Navbar = () => {
             className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
           >
             <div className="card-body">
-              <span className="font-bold text-lg">{cartItemsNum} {cartItemsNum === 1 ? 'item' : 'items'}</span>
+              <span className="font-bold text-lg">
+                {cartItemsNum} {cartItemsNum === 1 ? "item" : "items"}
+              </span>
               <span className="text-info">Subtotal: ${cartTotal}</span>
               <div className="card-actions">
-              <Link to="/checkout">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </Link>
+                <Link to="/checkout">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
@@ -63,11 +74,11 @@ const Navbar = () => {
           >
             <li>
               <Link to="/admin">
-            <span className="justify-between">
-              Admin Panel
-              <span className="badge ml-2">New</span>
-            </span>
-          </Link>
+                <span className="justify-between">
+                  Admin Panel
+                  <span className="badge ml-2">New</span>
+                </span>
+              </Link>
             </li>
             <li>
               <span>Settings</span>
@@ -81,6 +92,5 @@ const Navbar = () => {
     </div>
   );
 };
-
 
 export default Navbar;
