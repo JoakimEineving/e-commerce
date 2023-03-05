853e14db28118ea71fbe56d242ca9f7a0c00d4f8
joakimeineving <Joakim.eineving@gmail.com>
2023-03-05 16:08:27 +0100

    auth admin

---
+++ server/middleware/authAdmin.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const authAdmin = async (req, res, next) => {
+  try {
+    
+    const token = req.headers.authorization;
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decodedToken);
+    req.userData = { email: decodedToken.email, userId: decodedToken._id, isAdmin: decodedToken.isAdmin };
+    console.log(req.userData);
+    if (req.userData.isAdmin) {
+      next();
+    } else {
+      res.status(401).json({ message: 'Authentication failed!' });
+    }
+  } catch (error) {
+    res.status(401).json({ message: 'Authentication failed!' });
+  }
+};
+
+module.exports = authAdmin;
