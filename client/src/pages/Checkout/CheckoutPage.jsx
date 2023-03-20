0ce38a4c0524dbaae0506c375e50ac2d7c93b473
joakimeineving <Joakim.eineving@gmail.com>
2023-03-20 21:19:47 +0100

    load stripe

--- client/src/pages/Checkout/CheckoutPage.jsx
+++ client/src/pages/Checkout/CheckoutPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Navbar, Checkout } from "../../components/index";
-
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 const CheckoutPage = () => {
+  const stripePromise = loadStripe(
+    "pk_test_51MleTaD7q8sEvbFXtOJ1qJtFGLUps6Sloci1SpmJr0wEH6DmMtBo370gxzPM7r8tdJDt46NjfBqEbOhHF2HfhIp100lh5nQBKg"
+  );
   return (
     <div>
       <Navbar />
+      <Elements stripe={stripePromise}>
         <Checkout />
+        </Elements>
     </div>
   );
 };
