6a4cbdc8cfd0e052d3ae428b4cebdf71566dae0c
JoakimEineving <joakim.eineving@gmail.com>
2023-11-02 15:08:01 +0100

    add route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import OrderSuccessPage from "./pages/Checkout/OrderSuccessPage";
 import CheckoutPage from "./pages/Checkout/CheckoutPage";
 import OrdersPage from "./pages/User/OrdersPage";
 import PaymentPage from "./pages/Checkout/PaymentPage";
+import AdminManageProducts from "./pages/Admin/AdminManagePruducts";
 
 function App() {
   document.title = "E-commerce";
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin/manageproducts" element={<AdminManageProducts />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/checkout/shipping" element={<CheckoutPage />} />
         <Route path="/checkout/payment" element={<PaymentPage />} />
